5b74bac7616a11e4a99db52cc7770485bd6a1c2c
protected22 <simonjanse22@gmail.com>
2022-06-13 10:38:11 +0200

    Code cleanup + adding comments
    Code cleanup to make it better readable + adding comments to explain every f*cking bit of the code.

--- ButtonToMP3shield.ino
+++ ButtonToMP3shield.ino
@@ -21,61 +21,26 @@
 #include <vs1053_SdFat.h>
 #include <Bounce2.h> 
 
-/**
- * \breif Macro for the debounced NEXT pin, with pull-up
- */
+//bindnd variabalele to a Analog pin.
 #define B_TR1  A0
-
-/**
- * \breif Macro for the debounced STOP pin, with pull-up
- */
 #define B_TR2  A1
-
-/**
- * \breif Macro for the debounced PLAY pin, with pull-up
- */
 #define B_TR3  A2
 
-/**
- * \breif Macro for the Debounce Period [milliseconds]
- */
+//Define "debounce" period for every input.
 #define BUTTON_DEBOUNCE_PERIOD 20 //ms
 
-/**
- * \brief Object instancing the SdFat library.
- *
- * principal object for handling all SdCard functions.
- */
 SdFat sd;
-
-/**
- * \brief Object instancing the vs1053 library.
- *
- * principal object for handling all the attributes, members and functions for the library.
- */
 vs1053 MP3player;
 
-/**
- * \brief Object instancing the Next Button.
- */
+// bind the varabeles to the Bounce-libary
 Bounce b_TR1  = Bounce();
 
-/**
- * \brief Object instancing the Stop Button library.
- */
 Bounce b_TR2  = Bounce();
 
-/**
- * \brief Object instancing the Play Button library.
- */
 Bounce b_TR3  = Bounce();
 
-/**
- * \brief Index of the current track playing.
- *
- * Value indicates current playing track, used to populate "x" for playing the 
- * filename of "track00x.mp3" for track000.mp3 through track254.mp3
- */
+
+ //Connect the variables to a soundfile.
 int8_t TouchPad_1 = 0001;
 int8_t TouchPad_2 = 0002;
 int8_t TouchPad_3 = 0003;
@@ -83,18 +48,19 @@ int8_t TouchPad_4 = 0004;
 
 //------------------------------------------------------------------------------
 /**
- * \brief Setup the Arduino Chip's feature for our use.
- *
- * After Arduino's kernel has booted initialize basic features for this
- * application, such as Serial port and MP3player objects with .begin.
+ * \This is where the magic happens for the code to setup stuff
  */
+
+ //Make sure the Serial Monitor is on 115200 Baud
 void setup() {
   Serial.begin(115200);
 
+//Define the input for every variabele.
   pinMode(B_TR1, INPUT_PULLUP);
   pinMode(B_TR2, INPUT_PULLUP);
   pinMode(B_TR3, INPUT_PULLUP);
 
+//Connect the variables to the pin.
   //Attach button1 to pinmode TR1
   b_TR1.attach(B_TR1);
   b_TR1.interval(BUTTON_DEBOUNCE_PERIOD);
@@ -107,29 +73,26 @@ void setup() {
   b_TR3.attach(B_TR3);
   b_TR3.interval(BUTTON_DEBOUNCE_PERIOD);
 
+//debugging stuff
   if(!sd.begin(9, SPI_HALF_SPEED)) sd.initErrorHalt();
   if (!sd.chdir("/")) sd.errorHalt("sd.chdir");
 
+//Call MP3 shield
   MP3player.begin();
   MP3player.setVolume(10,10);
-  
-  Serial.println(F("Looking for Buttons to be depressed..."));
+
+  //Make sure the serial Monitor is ready for action.
+  Serial.println(F("Looking for Buttons to be pressed..."));
 }
 
 
 //------------------------------------------------------------------------------
 /**
- * \brief Main Loop the Arduino Chip
- *
- * This is called at the end of Arduino kernel's main loop before recycling.
- * And is where the user's is executed.
- *
- * \note If the means of refilling is not interrupt based then the
- * MP3player object is serviced with the availaible function.
+ * \Loop to let everything work again after something is pressed.
  */
 void loop() {
 
-// Below is only needed if not interrupt driven. Safe to remove if not using.
+// refil buffer of MP3Shield
 #if defined(USE_MP3_REFILL_MEANS) \
     && ( (USE_MP3_REFILL_MEANS == USE_MP3_SimpleTimer) \
     ||   (USE_MP3_REFILL_MEANS == USE_MP3_Polled)      )
@@ -137,6 +100,7 @@ void loop() {
   MP3player.available();
 #endif
 
+//Setup every button + their functionalities in a if-loop. 
 //Knop 1
   if (b_TR1.update()) {
     if (b_TR1.read() == LOW)  {
@@ -173,6 +137,60 @@ void loop() {
 
 
 
-   //Do something. Have fun with it.
+//We're no strangers to love
+//You know the rules and so do I (do I)
+//A full commitment's what I'm thinking of
+//You wouldn't get this from any other guy
+//I just wanna tell you how I'm feeling
+//Gotta make you understand
+//Never gonna give you up
+//Never gonna let you down
+//Never gonna run around and desert you
+//Never gonna make you cry
+//Never gonna say goodbye
+//Never gonna tell a lie and hurt you
+//We've known each other for so long
+//Your heart's been aching, but you're too shy to say it (say it)
+//Inside, we both know what's been going on (going on)
+//We know the game and we're gonna play it
+//And if you ask me how I'm feeling
+//Don't tell me you're too blind to see
+//Never gonna give you up
+//Never gonna let you down
+//Never gonna run around and desert you
+//Never gonna make you cry
+//Never gonna say goodbye
+//Never gonna tell a lie and hurt you
+//Never gonna give you up
+//Never gonna let you down
+//Never gonna run around and desert you
+//Never gonna make you cry
+//Never gonna say goodbye
+//Never gonna tell a lie and hurt you
+//We've known each other for so long
+//Your heart's been aching, but you're too shy to say it (to say it)
+//Inside, we both know what's been going on (going on)
+//We know the game and we're gonna play it
+//I just wanna tell you how I'm feeling
+//Gotta make you understand
+//Never gonna give you up
+//Never gonna let you down
+//Never gonna run around and desert you
+//Never gonna make you cry
+//Never gonna say goodbye
+//Never gonna tell a lie and hurt you
+//Never gonna give you up
+//Never gonna let you down
+//Never gonna run around and desert you
+//Never gonna make you cry
+//Never gonna say goodbye
+//Never gonna tell a lie and hurt you
+//Never gonna give you up
+//Never gonna let you down
+//Never gonna run around and desert you
+//Never gonna make you cry
+//Never gonna say goodbye
+//Never gonna tell a lie and hurt you
+
 
 }
